8960c441f7c60a5bb572f6279430d8a623800387
Dr. Asis Hallab <asis.hallab@gmail.com>
2019-04-13 12:28:17 +0200

    Made utils/cassandra_migrator_cli.js executable.

--- utils/cassandra_migrator_cli.js
+++ utils/cassandra_migrator_cli.js
@@ -4,11 +4,17 @@ const path = require('path')
 const cassandra = require('cassandra-driver')
 const cassandraConfig = require(path.join(__dirname, '..', 'config',
   'cassandra.config.json'))
-const cassandraClient = new cassandra.Client(cassandraConfig)
 const cassandraMigrator = require(path.join(__dirname, 'cassandra_migrator.js'))
 
 async function migrate() {
   try {
+    let cassandraConfigNoKeyspace = Object.assign({}, cassandraConfig)
+    delete cassandraConfigNoKeyspace.keyspace
+    let cassandraClient = new cassandra.Client(cassandraConfigNoKeyspace)
+    await cassandraMigrator.createKeyspaceIfNotExits({
+      cassandraClient: cassandraClient,
+      keyspace: cassandraConfig.keyspace
+    })
     await cassandraMigrator.migrate({
       cassandraClient: cassandraClient,
       path2MigrationsDir: path.resolve(path.join(__dirname, '..',
